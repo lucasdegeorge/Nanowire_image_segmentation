962d85e130f9156229fdbc4e0ed3dc97d3fbce52
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-01 16:33:32 +0200

    bug fixed and displaying

--- prepocessing/dataloader.py
+++ prepocessing/dataloader.py
@@ -4,6 +4,9 @@ import os
 from PIL import Image
 import torch
 import torchvision.transforms as T
+import matplotlib.pyplot as plt 
+import numpy as np
+import cv2 
 
 batch_size = 32 
 
@@ -11,6 +14,7 @@ labeled_image_dir = "C:/Users/lucas.degeorge/Documents/GitHub/Nanowire_image_seg
 annotation_dir = "C:/Users/lucas.degeorge/Documents/GitHub/Nanowire_image_segmentation/prepocessing/binary_masks"
 unlabeled_image_dir = "C:/Users/lucas.degeorge/Documents/GitHub/Nanowire_image_segmentation/prepocessing/unlabeled_images"
 
+filetype = '.png'
 
 #%% 
 
@@ -20,15 +24,13 @@ def load_labeled_data(image_dir, annotation_dir):
     labeled_images = []
     masks = []
     for filename in os.listdir(image_dir):
-        if filename.endswith('.png'):
+        if filename.endswith(filetype):
             image_path = os.path.join(image_dir, filename)
-            mask_path = os.path.join(annotation_dir, filename[:-4] + '.png')
+            mask_path = os.path.join(annotation_dir, filename[:-4] + '_mask.png')
             if os.path.isfile(mask_path):
                 labeled_images.append(image_path)
                 masks.append(mask_path)
     return labeled_images, masks
-
-print(load_labeled_data(labeled_image_dir, annotation_dir)[0])
 
 
 class LabeledDataset(torch.utils.data.Dataset):
@@ -56,7 +58,7 @@ class LabeledDataset(torch.utils.data.Dataset):
 def load_unlabeled_data(image_dir):
     unlabeled_images = []
     for filename in os.listdir(image_dir):
-        if filename.endswith('.jpg'):
+        if filename.endswith(filetype):
             image_path = os.path.join(image_dir, filename)
             unlabeled_images.append(image_path)
     return unlabeled_images
@@ -82,9 +84,59 @@ class UnlabeledDataset(torch.utils.data.Dataset):
 # Definition 
 
 labeled_dataset = LabeledDataset(labeled_image_dir, annotation_dir, transform=None)
-print(labeled_dataset.__len__())
 unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
-print(unlabeled_dataset.__len__())
 
-labeled_dataloader = torch.utils.data.DataLoader(labeled_dataset, batch_size=batch_size, shuffle=False)
-unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=False)
+labeled_dataloader = torch.utils.data.DataLoader(labeled_dataset, batch_size=batch_size, shuffle=True)
+unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True)
+
+
+#%% Display images and masks separated
+
+def display_image_with_mask(image, mask):
+    # Create a figure and axes
+    fig, ax = plt.subplots(1, 2)
+    
+    # Display the image
+    ax[0].imshow(image)
+    ax[0].set_title('Image')
+    
+    # Display the mask
+    ax[1].imshow(mask, cmap='gray')
+    ax[1].set_title('Mask')
+    
+    for axis in ax:
+        axis.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
+
+# Test : 
+image, mask = labeled_dataset[1]
+display_image_with_mask(image, mask)
+
+
+#%% Display image and mask overlayed
+
+def display_image_mask_overlayed(image, mask, alpha=0.6):
+    image = np.array(image.getdata()).reshape(image.size[0], image.size[1], 3)
+    mask = np.array(mask.getdata()).reshape(mask.size[0], mask.size[1])
+
+    color_map = {
+        0: [0, 0, 0],     # Background - Black
+        127: [255, 0, 0], # Droplet - Red
+        255: [0, 255, 0]  # Wire - Green
+    }
+
+    colored_mask = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
+    for value, color in color_map.items():
+        colored_mask[mask == value] = color
+    overlay = cv2.addWeighted(image.astype(np.uint8), 0.7, colored_mask.astype(np.uint8), 0.3, 0)
+
+    _, ax = plt.subplots()
+    ax.imshow(overlay)
+    ax.axis('off')
+    plt.show()
+
+# Tests : 
+image, mask = labeled_dataset[2]
+display_image_mask_overlayed(image, mask)
