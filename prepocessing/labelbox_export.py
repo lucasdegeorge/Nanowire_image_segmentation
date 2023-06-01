6afec1dd31c11c67c36d66c6deab2b0e3fa7a134
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-01 11:50:06 +0200

    Update labelbox_export.py

--- prepocessing/labelbox_export.py
+++ prepocessing/labelbox_export.py
@@ -9,24 +9,6 @@ project_id = "clicwkp7q06t607zg87vgerom"
 client = labelbox.client.Client(api_key=api_key)
 
 project = client.get_project(project_id)
-# dataset = client.get_dataset(dataset_id)
-
-# annotations = client.get_labeling_tasks(dataset=dataset)
-
-# for annotation in annotations:
-#     data = annotation.data
-#     # Assuming the annotation contains a segmentation mask
-#     mask = data.get('mask')
-
-#     if mask:
-#         # Convert the mask to a binary numpy array
-#         mask_array = np.array(mask, dtype=np.uint8)
-
-#         # Create a PIL Image from the binary mask array
-#         mask_image = Image.fromarray(mask_array * 255)
-
-#         # Save the mask image as a PNG file
-#         mask_image.save(f'{annotation.uid}.png')
 
 labels = project.export_v2(params={
 	"data_row_details": True,
@@ -37,3 +19,17 @@ labels = project.export_v2(params={
 	"label_details": True,
 	"interpolated_frames": True
   })
+
+for label in labels:
+    # Assuming the label contains a segmentation mask
+    mask = label.data.get('mask')
+
+    if mask:
+        # Convert the mask to a binary numpy array
+        mask_array = np.array(mask, dtype=np.uint8)
+
+        # Create a PIL Image from the binary mask array
+        mask_image = Image.fromarray(mask_array * 255)
+
+        # Save the mask image as a PNG file
+        mask_image.save(f'{label.uid}.png')
