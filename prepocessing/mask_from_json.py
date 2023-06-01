7c2e978052c3d2dd73b60b68f73d7b80fcd77903
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-01 16:14:57 +0200

    name convetion

--- prepocessing/mask_from_json.py
+++ prepocessing/mask_from_json.py
@@ -10,14 +10,15 @@ with open("annotations.json", "r") as read_file:
 
 all_file_names=list(data.keys())
 
-Files_in_directory = []
+files_in_directory = []
 for root, dirs, files in os.walk("labeled_images"):
     for filename in files:
-        Files_in_directory.append(filename)
+        files_in_directory.append(filename)
         
 for j in range(len(all_file_names)): # for each file
+    print(j)
     image_name=data[all_file_names[j]]['filename']
-    if image_name in Files_in_directory: 
+    if image_name in files_in_directory: 
          img = np.asarray(PIL.Image.open('labeled_images/'+image_name))    
     else:
         continue
@@ -37,6 +38,5 @@ for j in range(len(all_file_names)): # for each file
         img1 = cv2.drawContours(mask, [ab_wire], -1, 255, -1)
         img2 = cv2.drawContours(mask, [ab_droplet], -1, 127, -1)
         
-        # cv2.imwrite('binary_masks/%05.0f' % j +'.png',mask.astype(np.uint8))
-        cv2.imwrite('binary_masks/'+filename[:-4]+"_mask.png" % j,mask.astype(np.uint8))
+        cv2.imwrite('binary_masks/'+files_in_directory[j][:-4]+"_mask.png",mask.astype(np.uint8))
         
