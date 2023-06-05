6b09a9b506e42c57e694ae203f60967103e952e0
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-05 18:33:34 +0200

    updates on resent

--- model/encoder.py
+++ model/encoder.py
@@ -2,7 +2,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
 import os
+import numpy as np
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
