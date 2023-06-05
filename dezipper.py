9bb7b2d1e180bb6b5f531ed834d0eec49499bc6a
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-05 10:14:04 +0200

    correction and execution

--- dezipper.py
+++ dezipper.py
@@ -3,35 +3,32 @@
 import os
 import zipfile
 from skimage import io
+import czifile
+import matplotlib.pyplot as plt
 
-# Specify the input folder containing the zip files
 input_folder = "C:/Users/lucas.degeorge/Documents/Images/zip"
-
-# Specify the output folder for the extracted and converted files
+inter_folder = "C:/Users/lucas.degeorge/Documents/Images/labeled_apeer_inter"
 output_folder = "C:/Users/lucas.degeorge/Documents/Images/labeled_apeer"
 
-# Create the output folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
+#%% Extract the files from zip files
 
-# Loop through all the files in the input folder
+os.makedirs(inter_folder, exist_ok=True)
+
 for filename in os.listdir(input_folder):
     if filename.endswith('.zip'):
-        # Extract the zip file
-        zip_path = os.path.join(input_folder, filename)
+        zip_path = input_folder + "/" + filename
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(output_folder)
-
-        # Loop through the extracted files and convert czi to png
-        extracted_folder = os.path.join(output_folder, os.path.splitext(filename)[0])
-        for extracted_file in os.listdir(extracted_folder):
-            if extracted_file.endswith('.czi'):
-                czi_path = os.path.join(extracted_folder, extracted_file)
-                output_file = os.path.splitext(extracted_file)[0] + '.png'
-                output_path = os.path.join(output_folder, output_file)
-                image = io.imread(czi_path)
-                io.imsave(output_path, image)
-
-        # Remove the extracted folder
+            zip_ref.extractall(inter_folder)
         os.remove(zip_path)
 
-print('Extraction and conversion completed.')
+
+#%% Convert czi files into png files
+
+for filename in os.listdir(inter_folder):
+    if filename.endswith('.czi'):
+        czi_path = os.path.join(inter_folder, filename)
+        output_file = os.path.splitext(filename)[0] + '.png'
+        output_path = os.path.join(output_folder, output_file)
+        image = czifile.imread(czi_path)
+        # plt.imshow(image[0,0,0,0,:,:,0])
+        io.imsave(output_path, image[0,0,0,0,:,:,0])
