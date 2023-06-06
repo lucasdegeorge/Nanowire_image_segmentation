d4d7d64ecf6133ad1f281417ba43994ad83fd7d6
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-06 16:18:12 +0200

    changes for convertion in tensor

--- preprocessing/dataloader.py
+++ preprocessing/dataloader.py
@@ -21,15 +21,18 @@ filetype = '.png'
 # Labeled data and masks
 
 def load_labeled_data(image_dir, annotation_dir):
+    convert = T.ToTensor()
     labeled_images = []
     masks = []
     for filename in os.listdir(image_dir):
         if filename.endswith(filetype):
             image_path = os.path.join(image_dir, filename)
+            image = convert(Image.open(image_path))
             mask_path = os.path.join(annotation_dir, filename[:-4] + '_mask.png')
             if os.path.isfile(mask_path):
-                labeled_images.append(image_path)
-                masks.append(mask_path)
+                mask = convert(Image.open(mask_path).convert('L'))
+                labeled_images.append(image)
+                masks.append(mask)
     return labeled_images, masks
 
 
@@ -41,8 +44,8 @@ class LabeledDataset(torch.utils.data.Dataset):
         self.images, self.masks = load_labeled_data(image_dir, annotation_dir)
     
     def __getitem__(self, index):
-        image = Image.open(self.images[index]).convert('RGB')
-        mask = Image.open(self.masks[index]).convert('L')
+        image = self.images[index]
+        mask = self.masks[index]
         if self.transform is not None:
             image = self.transform(image)
             mask = self.transform(mask)
@@ -52,15 +55,16 @@ class LabeledDataset(torch.utils.data.Dataset):
         return len(self.images)
 
 
-
 # Unlabeled data 
 
 def load_unlabeled_data(image_dir):
+    convert = T.ToTensor()
     unlabeled_images = []
     for filename in os.listdir(image_dir):
         if filename.endswith(filetype):
             image_path = os.path.join(image_dir, filename)
-            unlabeled_images.append(image_path)
+            image = convert(Image.open(image_path))
+            unlabeled_images.append(image)
     return unlabeled_images
 
 
@@ -71,7 +75,7 @@ class UnlabeledDataset(torch.utils.data.Dataset):
         self.images = load_unlabeled_data(image_dir)
     
     def __getitem__(self, index):
-        image = Image.open(self.images[index]).convert('RGB')
+        image = self.images[index]
         if self.transform is not None:
             image = self.transform(image)
         return image
@@ -84,55 +88,52 @@ class UnlabeledDataset(torch.utils.data.Dataset):
 # Definition 
 
 labeled_dataset = LabeledDataset(labeled_image_dir, masks_dir, transform=None)
-unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
+# unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
 
 labeled_dataloader = torch.utils.data.DataLoader(labeled_dataset, batch_size=batch_size, shuffle=True)
-unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True)
+# unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True)
 
 
 #%% Display images and masks separated
 
 def display_image_with_mask(image, mask):
-    fig, ax = plt.subplots(1, 2)
-    
-    ax[0].imshow(image) # Display the image
-    ax[0].set_title('Image')
-    ax[1].imshow(mask, cmap='gray')     # Display the mask
-    ax[1].set_title('Mask')
-    
-    for axis in ax:
-        axis.axis('off')
-    
+    image = image.permute(1, 2, 0).numpy()
+    mask = mask.squeeze().numpy()
+
+    # Display the image
+    plt.subplot(1, 2, 1)
+    plt.imshow(image)
+    plt.title('Image')
+    plt.axis('off')
+    # Display the mask
+    plt.subplot(1, 2, 2)
+    plt.imshow(mask, cmap='gray')
+    plt.title('Mask')
+    plt.axis('off')
+
     plt.tight_layout()
     plt.show()
 
 # Test : 
-# image, mask = labeled_dataset[1]
-# display_image_with_mask(image, mask)
+image, mask = labeled_dataset[210]
+display_image_with_mask(image, mask)
 
 
 #%% Display image and mask overlayed
 
-def display_image_mask_overlayed(image, mask, alpha=0.6):
-    image = np.array(image.getdata()).reshape(image.size[0], image.size[1], 3)
-    mask = np.array(mask.getdata()).reshape(mask.size[0], mask.size[1])
+def display_image_mask_overlayed(image, mask, alpha=0.2):
+    image = image.permute(1, 2, 0).numpy()
+    mask = mask.squeeze().numpy()
 
-    color_map = {
-        0: [0, 0, 0],     # Background - Black
-        127: [255, 0, 0], # Droplet - Red
-        255: [0, 255, 0]  # Wire - Green
-    }
-
-    colored_mask = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
-    for value, color in color_map.items():
-        colored_mask[mask == value] = color
-    overlay = cv2.addWeighted(image.astype(np.uint8), 0.7, colored_mask.astype(np.uint8), 0.3, 0)
-
-    _, ax = plt.subplots()
-    ax.imshow(overlay)
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray')
     ax.axis('off')
+
+    cmap = plt.get_cmap('jet')
+    mask_colored = cmap(mask / mask.max()) 
+    ax.imshow(mask_colored, alpha=alpha)
     plt.show()
 
 # Tests : 
-# image, mask = labeled_dataset[78]
-# display_image_mask_overlayed(image, mask)
+image, mask = labeled_dataset[78]
+display_image_mask_overlayed(image, mask)
