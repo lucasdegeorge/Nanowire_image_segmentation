42444ec04645546601e39e1f78dcccd00f7182b1
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-06 11:33:01 +0200

    dilation added

--- model/resnet.py
+++ model/resnet.py
@@ -17,10 +17,10 @@ num_classes = 3
 
 # residual block for the resnet 18 and 34 
 class ResidualBlock_2sl(nn.Module): 
-    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None, dilation=1):
         super(ResidualBlock_2sl, self).__init__()
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = dilation, dilution = dilation),
                         nn.BatchNorm2d(out_channels),
                         nn.ReLU())
         self.conv2 = nn.Sequential(
@@ -42,24 +42,27 @@ class ResidualBlock_2sl(nn.Module):
     
 
 # residual block for the resnet 50, 101 and 152 
-class ResidualBlock_3sl(nn.Module): 
-    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+class ResidualBlock_3sl(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None, dilation=1):
         super(ResidualBlock_3sl, self).__init__()
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
-                        nn.BatchNorm2d(out_channels),
-                        nn.ReLU())
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=dilation, dilation=dilation),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
         self.conv2 = nn.Sequential(
-                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
-                        nn.BatchNorm2d(out_channels),
-                        nn.ReLU())
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
         self.conv3 = nn.Sequential(
-                        nn.Conv2d(out_channels, out_channels*4, kernel_size = 3, stride = 1, padding = 1),
-                        nn.BatchNorm2d(out_channels*4))   # je ne suis pas certains pour le *4 à verifier !!
+            nn.Conv2d(out_channels, out_channels * 4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels * 4)
+        )
         self.downsample = downsample
         self.relu = nn.ReLU()
         self.out_channels = out_channels
-        
+
     def forward(self, x):
         residual = x
         out = self.conv1(x)
@@ -70,42 +73,46 @@ class ResidualBlock_3sl(nn.Module):
         out += residual
         out = self.relu(out)
         return out
-    
+
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes = num_classes):
-        """ 
-            Layers: list with the number of blocks in each layer (stage)
-        """
+    def __init__(self, block, layers, num_classes=num_classes, isDilation=True, dilate_scale=8, multi_grid=(1, 2, 4)):
         super(ResNet, self).__init__()
         self.inplanes = 64
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, 64, kernel_size = 7, stride = 2, padding = 3),
-                        nn.BatchNorm2d(64),
-                        nn.ReLU())
-        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
+            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        if isDilation:
+            self.layer0 = self._make_layer(block, 64, layers[0], stride=1, dilation=multi_grid[0])
+            self.layer1 = self._make_layer(block, 128, layers[1], stride=2, dilation=dilate_scale // multi_grid[1])
+            self.layer2 = self._make_layer(block, 256, layers[2], stride=2, dilation=dilate_scale // multi_grid[2])
+            self.layer3 = self._make_layer(block, 512, layers[3], stride=2, dilation=dilate_scale)
+        else:
+            self.layer0 = self._make_layer(block, 64, layers[0], stride = 1)
+            self.layer1 = self._make_layer(block, 128, layers[1], stride = 2)
+            self.layer2 = self._make_layer(block, 256, layers[2], stride = 2)
+            self.layer3 = self._make_layer(block, 512, layers[3], stride = 2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512, num_classes)
-        
-    def _make_layer(self, block, planes, nb_blocks, stride=1):
+
+    def _make_layer(self, block, planes, nb_blocks, stride=1, dilation=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes: # if the dim of the residual does no match the dim of the output
+        if stride != 1 or self.inplanes != planes:
             downsample = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
                 nn.BatchNorm2d(planes),
             )
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(self.inplanes, planes, stride, downsample, dilation))
         self.inplanes = planes
         for i in range(1, nb_blocks):
-            layers.append(block(self.inplanes, planes))
+            layers.append(block(self.inplanes, planes, dilation=dilation))
 
         return nn.Sequential(*layers)
-    
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.maxpool(x)
@@ -121,17 +128,17 @@ class ResNet(nn.Module):
         return x
     
 
-def ResNet18():
-    return ResNet(ResidualBlock_2sl, [3,2,2,2])
+def ResNet18(isDilation = True):
+    return ResNet(ResidualBlock_2sl, [3,2,2,2], isDilation=isDilation)
 
-def ResNet50():
-    return ResNet(ResidualBlock_2sl, [3,4,6,3])
+def ResNet50(isDilation = True):
+    return ResNet(ResidualBlock_2sl, [3,4,6,3], isDilation=isDilation)
 
-def ResNet50():
-    return ResNet(ResidualBlock_3sl, [3,4,6,3])
+def ResNet50(isDilation = True):
+    return ResNet(ResidualBlock_3sl, [3,4,6,3], isDilation=isDilation)
 
-def ResNet101():
-    return ResNet(ResidualBlock_3sl, [3,4,23,3])
+def ResNet101(isDilation = True):
+    return ResNet(ResidualBlock_3sl, [3,4,23,3], isDilation=isDilation)
 
-def ResNet152():
-    return ResNet(ResidualBlock_3sl, [3,8,36,3])
+def ResNet152(isDilation = True):
+    return ResNet(ResidualBlock_3sl, [3,8,36,3], isDilation=isDilation)
