167ea97aa7961a2a33137c52903b09f68b95bb0a
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-08 11:22:16 +0200

    images saved as tensors in .pt files

--- preprocessing/dataloader.py
+++ preprocessing/dataloader.py
@@ -5,8 +5,6 @@ from PIL import Image
 import torch
 import torchvision.transforms as T
 import matplotlib.pyplot as plt 
-import numpy as np
-import cv2 
 
 batch_size = 32 
 
@@ -16,29 +14,53 @@ unlabeled_image_dir = "C:/Users/lucas.degeorge/Documents/Images/unlabeled_images
 
 filetype = '.png'
 
-#%% 
+#%% save tensors in .pt 
+
+def save_and_load(image_folder, mask_folder=None):
+    """ loads the images in the folders and save save in a .pt file using the same name"""
+    converter = T.ToTensor()
+    images = []
+    masks = []
+    # load and converter images 
+    for filename in os.listdir(image_folder):
+        if filename.endswith(filetype):
+            image_path = os.path.join(image_folder, filename)
+            image = Image.open(image_path).convert('L')
+            # if image.size != (1024,1024): image = image.resize((1024,1024))
+            image = converter(image)
+            images.append(image)
+            if mask_folder is not None:
+                mask_path = os.path.join(mask_folder, filename[:-4] + '_mask.png')
+                if os.path.isfile(mask_path):
+                    mask = Image.open(mask_path).convert('L')
+                    # if mask.size != (1024,1024): mask = mask.resize((1024,1024))
+                    mask = converter(mask)
+                    masks.append(mask)
+    # save tensors 
+    folder_name = image_folder.split("/")[-1] + ".pt"
+    torch.save(images, folder_name)
+    if mask_folder is not None:
+        folder_name = mask_folder.split("/")[-1] + ".pt"
+        torch.save(masks, folder_name)
+
+save_and_load(labeled_image_dir, masks_dir)
+save_and_load(unlabeled_image_dir, None)
+labeled_images = torch.load("labeled_images.pt")
+masks = torch.load("binary_masks.pt")
+unlabeled_images = torch.load("unlabeled_images.pt")
+
+#%% dataset and dataloader
 
 # Labeled data and masks
 
 def load_labeled_data(image_dir, annotation_dir):
-    convert = T.ToTensor()
-    labeled_images = []
-    masks = []
-    for filename in os.listdir(image_dir):
-        if filename.endswith(filetype):
-            image_path = os.path.join(image_dir, filename)
-            image = Image.open(image_path)
-            # if image.size != (1024,1024):
-            #     image = image.resize((1024,1024))
-            image = convert(image)
-            mask_path = os.path.join(annotation_dir, filename[:-4] + '_mask.png')
-            if os.path.isfile(mask_path):
-                mask = Image.open(mask_path).convert('L')
-                # if mask.size != (1024,1024):
-                #     mask = mask.resize((1024,1024))
-                mask = convert(mask)
-                labeled_images.append(image)
-                masks.append(mask)
+    try:
+        labeled_images = torch.load("labeled_images.pt")
+        masks = torch.load("binary_masks.pt")
+    except FileNotFoundError:
+        save_and_load(image_dir, annotation_dir)
+        labeled_images = torch.load("labeled_images.pt")
+        masks = torch.load("binary_masks.pt")
     return labeled_images, masks
 
 
@@ -64,13 +86,12 @@ class LabeledDataset(torch.utils.data.Dataset):
 # Unlabeled data 
 
 def load_unlabeled_data(image_dir):
-    convert = T.ToTensor()
-    unlabeled_images = []
-    for filename in os.listdir(image_dir):
-        if filename.endswith(filetype):
-            image_path = os.path.join(image_dir, filename)
-            image = convert(Image.open(image_path))
-            unlabeled_images.append(image)
+    try:
+        unlabeled_images = torch.load("unlabeled_images.pt")
+    except FileNotFoundError:
+        print("file unlabeled_images.pt not found")
+        save_and_load(image_dir, None)
+        unlabeled_images = torch.load("unlabeled_images.pt")
     return unlabeled_images
 
 
@@ -94,10 +115,10 @@ class UnlabeledDataset(torch.utils.data.Dataset):
 # Definition 
 
 labeled_dataset = LabeledDataset(labeled_image_dir, masks_dir, transform=None)
-# unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
+unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
 
 labeled_dataloader = torch.utils.data.DataLoader(labeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-# unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True)
+unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True)
 
 #%% Display images and masks separated
 
