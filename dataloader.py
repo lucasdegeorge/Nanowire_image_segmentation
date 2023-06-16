7b7b9ae00c0a22d5612d80ace57881d9d32c4028
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-16 11:14:02 +0200

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -202,9 +202,9 @@ def get_dataloaders(batch_size, labeled_image_dir=labeled_image_dir, masks_dir=m
     eval_labeled_dataset = eval_LabeledDataset(eval_images, eval_masks, transform=None)
     unlabeled_dataset = UnlabeledDataset(unlabeled_image_dir, transform=None)
 
-    train_labeled_dataloader = torch.utils.data.DataLoader(train_labeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=multiprocessing.cpu_count())
-    eval_labeled_dataloader = torch.utils.data.DataLoader(eval_labeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=multiprocessing.cpu_count())
-    unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True, num_workers=multiprocessing.cpu_count())
+    train_labeled_dataloader = torch.utils.data.DataLoader(train_labeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True) #, num_workers=multiprocessing.cpu_count())
+    eval_labeled_dataloader = torch.utils.data.DataLoader(eval_labeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True) #, num_workers=multiprocessing.cpu_count())
+    unlabeled_dataloader = torch.utils.data.DataLoader(unlabeled_dataset, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True) #, num_workers=multiprocessing.cpu_count())
 
     return train_labeled_dataloader, eval_labeled_dataloader,  unlabeled_dataloader
 
