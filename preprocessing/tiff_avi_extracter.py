011f1257d2a7b189b2ab7f223accaa679b9d57d5
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-06-22 10:05:51 +0200

    new nanomax images extraction

--- preprocessing/tiff_avi_extracter.py
+++ preprocessing/tiff_avi_extracter.py
@@ -35,7 +35,7 @@ def extract_frames_from_avi(avi_file, output_folder, output_format='png'):
         # Check if the frame was read successfully
         if ret:
             os.makedirs(output_folder, exist_ok=True)
-            output_file = os.path.join(output_folder, f'frame_{frame_count}.{output_format}')
+            output_file = os.path.join(output_folder, avi_file.split("/")[-1].split(".")[0] + f'_frame_{frame_count}.{output_format}')
             cv2.imwrite(output_file, frame)
             # print(f'Saved frame {frame_count} as {output_file}')
             frame_count += 1
@@ -43,6 +43,20 @@ def extract_frames_from_avi(avi_file, output_folder, output_format='png'):
             break
     video.release()
 
-avi_file_path = "C:/Users/lucas.degeorge/Documents/Images/videos_to_convert/file" # replace file
-output_folder = "C:/Users/lucas.degeorge/Documents/Images/unlabeled_images"
-extract_frames_from_avi(avi_file_path, output_folder, output_format='png')
+avi_folder_path = "D:/Images_nanomax"
+output_folder = "D:/Images_nanomax/new_images"
+
+folders = []
+for file in os.listdir(avi_folder_path):
+        d = os.path.join(avi_folder_path, file)
+        if os.path.isdir(d):
+             folders.append(d)
+
+folders.remove('D:/Images_nanomax\\new_images')
+
+for folder in folders:
+     for filename in os.listdir(folder):
+        if filename.endswith(".avi"):
+            # print(folder + "/" + filename)
+            extract_frames_from_avi(folder + "/" + filename, output_folder + "/" + folder.split("\\")[-1], output_format='png')
+            print(filename, "completed")
