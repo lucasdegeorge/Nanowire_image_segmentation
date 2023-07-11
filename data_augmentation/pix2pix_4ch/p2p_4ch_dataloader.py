1ccb7bd57861065bd55ae6f0408dacadc32f8337
Lucas Degeorge <103430109+lucasdegeorge@users.noreply.github.com>
2023-07-11 16:46:39 +0200

    Update p2p_4ch_dataloader.py

--- data_augmentation/pix2pix_4ch/p2p_4ch_dataloader.py
+++ data_augmentation/pix2pix_4ch/p2p_4ch_dataloader.py
@@ -1,77 +1,106 @@
 #%% 
 import os
-from PIL import Image
 import torch
-import torchvision.transforms as T
-import matplotlib.pyplot as plt 
-from torch.utils.data import Dataset, DataLoader
-import json
+from torchvision import transforms
+from PIL import Image
+import sys 
 
-with open("C:/Users/lucas.degeorge/Documents/GitHub/Nanowire_image_segmentation/parameters.json", 'r') as f:
-    arguments = json.load(f)
-    device = arguments["device"]
-    device = torch.device(device)
+sys.path.append("C:/Users/lucas.degeorge/Documents/GitHub/Nanowire_image_segmentation")
+from preprocessing.size_reduction import resize
 
-c2n = True
+## Resize the labeled images 
+image_path = "C:/Users/lucas.degeorge/Documents/Images/labeled_images"
+mask_path = "C:/Users/lucas.degeorge/Documents/Images/binary_masks"
+output_path_images = "C:/Users/lucas.degeorge/Documents/Images/resized_images/resized_labeled_images"
+output_path_masks = "C:/Users/lucas.degeorge/Documents/Images/resized_images/resized_masks"
+where_write = "C:/Users/lucas.degeorge/Documents/Images/resized_images"
 
-if c2n:
-    image_folder = "C:/Users/lucas.degeorge/Documents/Images/labeled_images"
-    mask_folder = "C:/Users/lucas.degeorge/Documents/Images/binary_masks"
-else:
-    image_folder = "C:/Users/lucas/Desktop/labeled_images"
-    mask_folder = "C:/Users/lucas/Desktop/binary_masks"
-
-filetype = '.png'
+# resize(image_path, mask_path, output_path_A, output_path_B)
 
 #%% 
 
-class ImageMaskDataset(Dataset):
-    def __init__(self, images, masks, transform=None):
-        self.images = images
-        self.masks = masks
-        # self.images = [ t.to(device) for t in images]
-        # self.masks = [ t.to(device) for t in masks]
+## Combine images and masks
+def combine_image_mask(image_folder, mask_folder, one_batch=False):
+    image_files = os.listdir(image_folder)
+
+    combined_tensors = []
+
+    for image_file in image_files:
+        image_path = os.path.join(image_folder, image_file)
+        mask_file = image_file.replace(".png", "_mask.png")
+        mask_path = os.path.join(mask_folder, mask_file)
+
+        image = Image.open(image_path).convert("L")
+        image = transforms.ToTensor()(image)
+        mask = Image.open(mask_path).convert("L")
+        mask = transforms.ToTensor()(mask)
+
+        if one_batch:
+            image = image.unsqueeze(0)
+            mask = mask.unsqueeze(0)
+
+            # Concatenate image and mask tensors
+            combined_tensor = torch.cat((image, mask), dim=1)
+            combined_tensors.append(combined_tensor)
+        
+        else:
+            combined_tensor = torch.cat((image, mask), dim=0)
+            combined_tensors.append(combined_tensor)
+
+    if one_batch: return torch.cat(combined_tensors, dim=0) # Stack all combined tensors along the batch dimension
+    else: return combined_tensors
+
+
+# separate image and mask
+def separate_image_mask(combined_tensor, return_PIL=False):
+    # Split the combined tensor into image and mask tensors
+    image = combined_tensor[0:1, :, :]
+    mask = combined_tensor[1:2, :, :]
+
+    image = image.squeeze(1)
+    mask = mask.squeeze(1)
+
+    if return_PIL:
+        image = transforms.ToPILImage()(image)
+        mask = transforms.ToPILImage()(mask)
+
+    return image, mask
+
+
+# tests : 
+# combined_tensors = combine_image_mask(output_path_images, output_path_masks, one_batch=False)
+# image, mask = separate_image_mask(combined_tensors[0], True)
+
+def data_processing(image_folder, mask_folder, output_A, output_B, where_write):
+    """ For all the images of image_folder: resize, combine with mask and save as a .pt file """
+    resize(image_folder, mask_folder, output_A, output_B)
+    combined_tensors = combine_image_mask(output_A, output_B, False)
+    torch.save(combined_tensors, where_write + "/combined_data")
+
+# data_processing(image_path, mask_path, output_path_images, output_path_masks, where_write)
+
+
+class CombinedDataset(torch.utils.data.Dataset):
+    def __init__(self, pt_path, transform=None):
         self.transform = transform
-
-    def __len__(self):
-        return len(self.images)
-
+        try:
+            self.combined_images = torch.load(pt_path)
+        except FileNotFoundError:
+            print("file not found: ", pt_path)
+            raise FileNotFoundError
+    
     def __getitem__(self, index):
-        image = self.images[index]
-        mask = self.masks[index]
+        image = self.combined_images[index]
         if self.transform is not None:
             image = self.transform(image)
-            mask = self.transform(mask)
-        return image, mask
+        return image
     
+    def __len__(self):
+        return len(self.combined_images)
 
-def create_dataloader(image_folder, mask_folder, batch_size, shuffle=True, pin_memory=True):
 
-    # load and convet images
-    converter = T.ToTensor()
-    images = []
-    masks = []
-    # load and converter images 
-    for filename in os.listdir(image_folder):
-        if filename.endswith(filetype):
-            image_path = os.path.join(image_folder, filename)
-            image = Image.open(image_path).convert("L")
-            image = converter(image)
-            images.append(image)
-
-            if mask_folder is not None:
-                mask_path = os.path.join(mask_folder, filename[:-4] + '_mask.png')
-                if os.path.isfile(mask_path):
-                    mask = Image.open(mask_path).convert("L")
-                    mask = T.functional.to_tensor(mask) * 255
-                    # mask = mask.to(torch.uint8) 
-                    masks.append(mask)
-    # return images, masks
-
-    dataset = ImageMaskDataset(images, masks)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=True, pin_memory=pin_memory)
-    print("dataloader ok")
-    return dataloader
-
-# images, masks = create_dataloader(image_folder, mask_folder, batch_size=2, shuffle=True, pin_memory=True)
+def get_dataloader(pt_file, batch_size, shuffle=True, pin_memory=True):
+    transform = None #transforms.Compose([transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    dataset = CombinedDataset(pt_file, transform)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=pin_memory)
 
